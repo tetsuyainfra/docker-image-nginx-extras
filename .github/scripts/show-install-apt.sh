b8b7729def522ff78b919c1fcd72ca24accddf93
tetsuyainfra <tetsuyainfra@gmail.com>
2025-06-17 17:49:10 +0900

    gh actions: インストールパッケージ等の設定をworkflowで行うようにした

--- .github/scripts/show-install-apt.sh
+++ .github/scripts/show-install-apt.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-DOCKER_IMAGE="ubuntu:24.04"
-INSTALL_PACKAGES="nginx nginx-extras"
+DOCKER_IMAGE="${DOCKER_BASE_IMAGE}"
+INSTALL_PACKAGES="${INSTALL_PACKAGES}"
 
 # pullして最新にする
 docker pull $DOCKER_IMAGE > /dev/null 2>&1
 
 docker run --pull never --rm $DOCKER_IMAGE\
-    bash -c "dpkg-query -W -f='\${binary:Package} \${Version}\n'"
+    bash -c "dpkg-query -W -f='\${binary:Package} \${Version}\n' | sort"
 
 # show upgrade packages/ install pacakge
 docker run --pull never --rm $DOCKER_IMAGE \
     bash -c "apt-get update > /dev/null && \
                 echo ----- && \
-                apt list --upgradable 2>/dev/null | grep -v 'Listing...' | tr / ' ' | awk '{print \$1, \$3}' && \
+                apt list --upgradable 2>/dev/null | grep -v 'Listing...' | tr / ' ' | awk '{print \$1, \$3}' | sort && \
                 echo ----- && \
-                apt-get install -s $INSTALL_PACKAGES | grep ^Inst | tr -d '(' | awk '{print \$2, \$3}'
+                apt-get install -s $INSTALL_PACKAGES | grep ^Inst | tr -d '(' | awk '{print \$2, \$3}' | sort
             "
 
 # show nginx version
